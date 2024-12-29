88b31388ac6c62b8800001294f700a0f720abfe9
LegendaryAman <amanmaurya8806@gmail.com>
2024-12-29 18:30:35 +0530

    two

--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -18,7 +18,7 @@ const TextFrom = (props) => {
         setText(event.target.value)
     }
 
-    const [text, setText] = useState('Enter text here');
+    const [text, setText] = useState("");
     // text = "new text"; // Wrong way to change the state
     // setText("new Text"); // Correct  way to change trhe state
     
@@ -27,7 +27,7 @@ const TextFrom = (props) => {
     <div>
       <form action="post">
         <h1>{props.heading}</h1>
-        <textarea className="textarea" id="t1" cols="200" rows="5" onChange={handleOnChange}></textarea>
+        <textarea className="textarea" id="t1" cols="200" rows="5" value={text} onChange={handleOnChange}></textarea>
       </form>
       <button onClick={handleUpClick}>Convert To UpperCase</button>
       <button onClick={handleLoClick}>Convert To LowerCase</button>
@@ -36,7 +36,7 @@ const TextFrom = (props) => {
       <h1>Your text summary</h1>
     <p>{text.split(" ").length} words and {text.length} Characters</p>
     <h1>Time to Read</h1>
-    <p>{ 0.008 * text.split(" ").length }</p>
+    <p>{(text.split(" ").length)*(0.008)} Minutes</p>
     <h1>Preview</h1>
     <p>{text}</p>
     </div>
