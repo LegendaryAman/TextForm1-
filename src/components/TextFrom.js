47b0c0b35894ffedb8a1674e229369ed0fbc9ad6
LegendaryAman <amanmaurya8806@gmail.com>
2024-12-29 16:25:10 +0530

    one

--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const TextFrom = (props) => {
+
+  const handleUpClick = ()=>{
+    //console.log("Uppercase was clicked" + text)
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const handleLoClick = ()=>{
+    //console.log("Uppercase was clicked" + text)
+    let newText = text.toLowerCase();
+    setText(newText)
+  }
+
+    const handleOnChange = (event)=>{
+        //console.log("onchange")
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState('Enter text here');
+    // text = "new text"; // Wrong way to change the state
+    // setText("new Text"); // Correct  way to change trhe state
+    
   return (
+    <>
     <div>
       <form action="post">
         <h1>{props.heading}</h1>
-        <textarea name="textarea" id="t1" cols="200" rows="5"></textarea>
+        <textarea className="textarea" id="t1" cols="200" rows="5" onChange={handleOnChange}></textarea>
       </form>
+      <button onClick={handleUpClick}>Convert To UpperCase</button>
+      <button onClick={handleLoClick}>Convert To LowerCase</button>
     </div>
+    <div className='count'>
+      <h1>Your text summary</h1>
+    <p>{text.split(" ").length} words and {text.length} Characters</p>
+    <h1>Time to Read</h1>
+    <p>{ 0.008 * text.split(" ").length }</p>
+    <h1>Preview</h1>
+    <p>{text}</p>
+    </div>
+    </>
   )
 }
 
